0d636d302271d62e88d6dc44d5351ca0b5ad9429
victor <spayn112@gmail.com>
2022-03-16 22:39:20 +0400

    cs fixes

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -26,11 +26,11 @@ final class GildedRose
     {
         foreach ($this->items as $item) {
             $item = match ($item->name) {
-                'Aged Brie'                                 => new AgedBrie($item),
-                'Sulfuras, Hand of Ragnaros'                => new Sulfras($item),
+                'Aged Brie' => new AgedBrie($item),
+                'Sulfuras, Hand of Ragnaros' => new Sulfras($item),
                 'Backstage passes to a TAFKAL80ETC concert' => new Backstage($item),
-                'Conjured Mana Cake'                        => new Conjured($item),
-                default                                     => new Standard($item),
+                'Conjured Mana Cake' => new Conjured($item),
+                default => new Standard($item),
             };
             $item->updateQuality();
         }
--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -19,92 +19,92 @@ class GildedRoseTest extends TestCase
         $gildedRose = new GildedRose($items);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(0, $items[0]->sell_in);
-        $this->assertEquals(9, $items[0]->quality);
-        $this->assertEquals(0, $items[1]->sell_in);
-        $this->assertEquals(9, $items[1]->quality);
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(9, $items[0]->quality);
+        $this->assertSame(0, $items[1]->sell_in);
+        $this->assertSame(9, $items[1]->quality);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(-1, $items[0]->sell_in);
-        $this->assertEquals(7, $items[0]->quality);
-        $this->assertEquals(-1, $items[1]->sell_in);
-        $this->assertEquals(7, $items[1]->quality);
+        $this->assertSame(-1, $items[0]->sell_in);
+        $this->assertSame(7, $items[0]->quality);
+        $this->assertSame(-1, $items[1]->sell_in);
+        $this->assertSame(7, $items[1]->quality);
     }
 
     public function testSulfuras(): void
     {
         $items = [
             new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-            new Item('Sulfuras, Hand of Ragnaros', -1, 50)
+            new Item('Sulfuras, Hand of Ragnaros', -1, 50),
         ];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals(0, $items[0]->sell_in);
-        $this->assertEquals(80, $items[0]->quality);
-        $this->assertEquals(-1, $items[1]->sell_in);
-        $this->assertEquals(80, $items[1]->quality);
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(80, $items[0]->quality);
+        $this->assertSame(-1, $items[1]->sell_in);
+        $this->assertSame(80, $items[1]->quality);
     }
 
     public function testAgedBrie(): void
     {
         $items = [
             new Item('Aged Brie', 1, 1),
-            new Item('Aged Brie', -1, 49)
+            new Item('Aged Brie', -1, 49),
         ];
         $gildedRose = new GildedRose($items);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(0, $items[0]->sell_in);
-        $this->assertEquals(2, $items[0]->quality);
-        $this->assertEquals(-2, $items[1]->sell_in);
-        $this->assertEquals(50, $items[1]->quality);
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(2, $items[0]->quality);
+        $this->assertSame(-2, $items[1]->sell_in);
+        $this->assertSame(50, $items[1]->quality);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(-1, $items[0]->sell_in);
-        $this->assertEquals(4, $items[0]->quality);
-        $this->assertEquals(-3, $items[1]->sell_in);
-        $this->assertEquals(50, $items[1]->quality);
+        $this->assertSame(-1, $items[0]->sell_in);
+        $this->assertSame(4, $items[0]->quality);
+        $this->assertSame(-3, $items[1]->sell_in);
+        $this->assertSame(50, $items[1]->quality);
     }
 
     public function testBackstagePasses(): void
     {
         $items = [
             new Item('Backstage passes to a TAFKAL80ETC concert', 11, 24),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 1, 49)
+            new Item('Backstage passes to a TAFKAL80ETC concert', 1, 49),
         ];
         $gildedRose = new GildedRose($items);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(10, $items[0]->sell_in);
-        $this->assertEquals(25, $items[0]->quality);
-        $this->assertEquals(0, $items[1]->sell_in);
-        $this->assertEquals(50, $items[1]->quality);
+        $this->assertSame(10, $items[0]->sell_in);
+        $this->assertSame(25, $items[0]->quality);
+        $this->assertSame(0, $items[1]->sell_in);
+        $this->assertSame(50, $items[1]->quality);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(9, $items[0]->sell_in);
-        $this->assertEquals(27, $items[0]->quality);
-        $this->assertEquals(-1, $items[1]->sell_in);
-        $this->assertEquals(0, $items[1]->quality);
+        $this->assertSame(9, $items[0]->sell_in);
+        $this->assertSame(27, $items[0]->quality);
+        $this->assertSame(-1, $items[1]->sell_in);
+        $this->assertSame(0, $items[1]->quality);
     }
 
     public function testConjured(): void
     {
         $items = [
             new Item('Conjured Mana Cake', 3, 6),
-            new Item('Conjured Mana Cake', 0, 3)
+            new Item('Conjured Mana Cake', 0, 3),
         ];
         $gildedRose = new GildedRose($items);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(2, $items[0]->sell_in);
-        $this->assertEquals(4, $items[0]->quality);
-        $this->assertEquals(-1, $items[1]->sell_in);
-        $this->assertEquals(0, $items[1]->quality);
+        $this->assertSame(2, $items[0]->sell_in);
+        $this->assertSame(4, $items[0]->quality);
+        $this->assertSame(-1, $items[1]->sell_in);
+        $this->assertSame(0, $items[1]->quality);
 
         $gildedRose->updateQuality();
-        $this->assertEquals(1, $items[0]->sell_in);
-        $this->assertEquals(2, $items[0]->quality);
-        $this->assertEquals(-2, $items[1]->sell_in);
-        $this->assertEquals(0, $items[1]->quality);
+        $this->assertSame(1, $items[0]->sell_in);
+        $this->assertSame(2, $items[0]->quality);
+        $this->assertSame(-2, $items[1]->sell_in);
+        $this->assertSame(0, $items[1]->quality);
     }
 }
